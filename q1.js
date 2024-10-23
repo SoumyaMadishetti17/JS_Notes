169f2882842e879fe4e8d88bd0c2ff95d49534f1
SoumyaMadishetti17 <soumyamadishettimsd17@gmail.com>
2024-10-23 23:56:11 +0530

    new

--- q1.js
+++ q1.js
@@ -26,3 +26,13 @@ if(res>100){
 }else{
     console.log('no');
 }
+
+//q4
+let n = 5; 
+for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '*';
+    }
+    console.log(row);
+}
