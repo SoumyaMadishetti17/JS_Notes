1616e0c9703f6fca52e2f5db2526637e9e7ecbe0
SoumyaMadishetti17 <soumyamadishettimsd17@gmail.com>
2024-10-23 23:48:51 +0530

    concatenate

--- q1.js
+++ q1.js
@@ -2,3 +2,15 @@ let first_name='janu'
 let last_name='dew'
 let full_name=first_name+last_name
 console.log(`Hello, ${full_name}`);
+
+
+// q2
+let num1=3
+let num2=3
+console.log(num1*num2);//9
+console.log(num1+num2);//6
+console.log(num1-num2);//0
+console.log(num1/num2);//1
+console.log(num1==num2);//true
+console.log(num1===num2);//true
+console.log(num1%num2);//0
